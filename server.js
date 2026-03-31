aa62a7e790ad6feccb87b2632fb4eec24f244e17
qasem-moh <qasemcoderjo2020@gmail.com>
2026-03-31 13:04:07 -0700

    r

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const attendanceRoutes = require('./routes/attendance');
+const attendanceRoutes = require('./src/routes/attendance');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
